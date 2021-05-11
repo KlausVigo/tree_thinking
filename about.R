09c5a3c04b8a80bb94bfcd1d0479deaf0aea0531
Klaus Schliep <klaus.schliep@gmail.com>
2021-05-11 17:21:18 +0200

    spelling

--- about.R
+++ about.R
@@ -4,7 +4,7 @@ HTML("<h1>Tree Thinking</h1>
     <p>tree_thinking provides some small quizzes to test your knowledge about phylogenetic trees inspired by the book 'Tree thinking'.</p>
     <p>It is possible to run it from within R:</p>
     <p><code>shiny::runGitHub('tree_thinking', 'KlausVigo')</code></p>
-    <p>You can launched it also via the 
+    <p>You can launched it also via 
     <a ref='http://klash.shinyapps.io/tree_thinking/'>http://klash.shinyapps.io/tree_thinking/</a> from your web browser.</p>
     <p>The source code shinyTreeviewer is on <a href='https://github.com/KlausVigo/tree_thinking/'>GitHub</a>. tree_thinking
     is in early development so I am happy to recieve suggestions or bug reports. 
